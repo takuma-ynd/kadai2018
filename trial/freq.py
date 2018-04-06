d088a69a1091c42c7ee408591846ece1dc742b97
takuma-ynd <takuma-ynd@users.noreply.github.com>
2018-04-06 15:39:43 +0900

    add comment about the order of priority

--- trial/freq.py
+++ trial/freq.py
@@ -9,6 +9,7 @@ with open(sys.argv[1], "r") as f:
             else:
                 d[word] = 1
 
+# freq, alphabetical-orderの順に優先度が高いsortをする．
 output = sorted(d.items(), key=lambda x: (-x[1], x[0]))
 for word, freq in output:
     print("".join((" " * 5, "{} {}".format(freq, word))))
