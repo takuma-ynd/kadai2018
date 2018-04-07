12f92965317cd0db46963d6468dbcf97d6cf0de0
takuma-ynd <takuma-ynd@users.noreply.github.com>
2018-04-07 09:45:32 +0900

    add word_tokenize_file function

--- preprocess/word_tokenize.py
+++ preprocess/word_tokenize.py
@@ -1,12 +1,19 @@
 import sys
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-with open(sys.argv[1], 'r') as f:
-    text = f.read()
-sentences = sent_tokenize(text)
-words = []
-for sentence in sentences:
-    words.extend(word_tokenize(sentence))
 
-for e in words:
-    print(e)
+def word_tokenize_file(file_name):
+    with open(file_name, 'r') as f:
+        text = f.read()
+    sentences = sent_tokenize(text)
+    words = []
+    for sentence in sentences:
+        words.extend(word_tokenize(sentence))
+    return words
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2
+    words = word_tokenize_file(sys.argv[1])
+    for e in words:
+        print(e)
